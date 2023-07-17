c4e51d322c2b4b3f836932dea9d8c65be3cf4032
levchsergey86 <116516113+levchsergey86@users.noreply.github.com>
2023-07-17 21:54:03 +0300

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,79 @@
-export const App = () => {
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { nanoid } from 'nanoid';
+import ContactForm from './ContactForm/ContactForm';
+import FilterContacts from './FilterContacts/FilterContacts';
+import ContactList from './ContactList/ContactList';
+import { addContact, deleteContact } from '../redux/contactsSlice';
+import { setFilter } from '../redux/filterSlice';
+import styled from './App.module.css';
+
+const App = () => {
+  const contacts = useSelector(state => state.contacts);
+  const filter = useSelector(state => state.filter);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const storedContacts = localStorage.getItem('contacts');
+    if (storedContacts) {
+      const parsedContacts = JSON.parse(storedContacts);
+      if (parsedContacts.length > 0) {
+        dispatch(addContact(parsedContacts));
+      }
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const handleDeleteContact = id => {
+    dispatch(deleteContact(id));
+  };
+
+  const handleAddContact = (name, number) => {
+    const isContactExists = contacts.some(
+      contact =>
+        contact.name &&
+        contact.name.toLowerCase() === name.toLowerCase() &&
+        contact.number === number
+    );
+
+    if (isContactExists) {
+      alert('Contact with the same name or number already exists.');
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+
+    dispatch(addContact(newContact));
+  };
+
+  const handleFilterChange = event => {
+    dispatch(setFilter(event.target.value));
+  };
+
+  const filteredContacts = contacts.filter(
+    contact =>
+      contact.name && contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={styled.container}>
+      <h1>PHONE BOOK</h1>
+      <ContactForm addContact={handleAddContact} />
+      <h2>Contacts:</h2>
+      <FilterContacts handleFilterChange={handleFilterChange} />
+      <ContactList
+        filteredContacts={filteredContacts}
+        deleteContact={handleDeleteContact}
+      />
     </div>
   );
 };
+
+export default App;
---
+++ src/redux/store.js
@@ -0,0 +1,38 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import contactsReducer from './contactsSlice';
+import filterReducer from './filterSlice';
+import persistConfig from './persistConfig';
+
+const rootReducer = {
+  contacts: contactsReducer,
+  filter: filterReducer,
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers(rootReducer)
+);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
